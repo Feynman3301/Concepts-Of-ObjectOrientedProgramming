2e5921859911547a98e7393c94ffa0db5f74c023
Feynman3301 <58468033+Feynman3301@users.noreply.github.com>
2019-12-18 21:50:40 +0530

    Update Example of Polymorphism.java

--- Example of Polymorphism.java
+++ Example of Polymorphism.java
@@ -1,1 +1,3 @@
-class 
+/*used by the means of: methodName() or methodName(int x, float y,...) in static form 
+and as parentClass.methodName() or childClass.methodName() in dynamic form*/
+class Polymorpism
