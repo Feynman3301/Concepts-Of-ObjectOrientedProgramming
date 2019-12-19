a055147b45ab54a8b75c6a093c8894f73e8d7154
Feynman3301 <58468033+Feynman3301@users.noreply.github.com>
2019-12-19 17:02:34 +0530

    Update Example of Encapsulation.java

--- Example of Encapsulation.java
+++ Example of Encapsulation.java
@@ -1,4 +1,55 @@
 //used by the means of:private getData() setData()
 class Encapsulation
 {
+  public String Name="alex";
+  private int mm,dd,yyyy;
+  private String ssn;
+  private void ChangeSSN(String s)
+  {
+    ssn=s;
+  }
+  private void ChangeDOB(int a,int b,int c)
+  {
+    mm=a;dd=b;yyyy=c;
+  }
+  public int getDate(String s)
+  { 
+    return dd;
+  }
+  public int getMonth(String s)
+  {
+    return mm;
+  }
+  public int getYear(String s)
+  {
+    return yyyy;
+  }
+  public String getSSN(String s)
+  {
+    return ssn;
+  }
+  public static void main(String args[])
+  {
+    Encapsulation ob=new Encapsulation();
+    ob.ChangeSSN("123453231");
+    ob.ChangeDOB(05,09,1458);
+    ob.getDate("alex");
+    ob.getMonth("alex");
+    ob.getYear("alex");
+    ob.getSSN("alex");
+  }
+}
+class Access extends Encapsulation
+{
+    Access ob=new Access();
+    super.ob.ChangeSSN("123453231");
+     super.ob.ChangeDOB(05,09,1458);
+     super.ob.getDate("alex");
+     super.ob.getMonth("alex");
+     super.ob.getYear("alex");
+     super.ob.getSSN("alex");
+}
+ 
+  
+  
   
