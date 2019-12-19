dd81e32565a35da35cfe21ea5f3606f125435a9c
Feynman3301 <58468033+Feynman3301@users.noreply.github.com>
2019-12-19 18:18:48 +0530

    Update Example of Polymorphism.java

--- Example of Polymorphism.java
+++ Example of Polymorphism.java
@@ -1,3 +1,69 @@
 /*used by the means of: methodName() or methodName(int x, float y,...) in static form 
 and as parentClass.methodName() or childClass.methodName() in dynamic form*/
-class Polymorpism
+class Polymorphism
+{
+    private float Sum(float a,float b)
+  {
+    return a+b;
+  }
+  private int Sum(int a, int b)
+  {
+    return a+b;
+  }
+ public void Input()
+ {
+     Polymorphism ob=new Polymorphism();
+    System.out.println("For accurate sum");
+    float a=ob.Sum(904.45f,654.2132f);
+   System.out.println("For accurate sum");
+    int b=ob.Sum(904,654);
+ }
+}
+class Multiply extends Polymorphism
+{
+private float Multiply(float a,float b)
+  {
+    return a*b;
+  }
+  private int Multiply(int a, int b)
+  {
+    return a*b;
+  }
+ public void Input()
+ {
+    Multiply x=new Multiply();
+    System.out.println("For accurate sum");
+    float a=x.Multiply(904.45f,654.2132f);
+   System.out.println("For accurate sum");
+    int b=x.Multiply(904,654);
+ }
+}
+class Divide extends Multiply
+{
+private float Divide(float a,float b)
+  {
+    return a/b;
+  }
+  private int Divide(int a, int b)
+  {
+    return a/b;
+  }
+ public void Input()
+ {
+    Divide x=new Divide();
+    System.out.println("For accurate sum");
+    float a=x.Divide(904.45f,654.2132f);
+   System.out.println("For accurate sum");
+    int b=x.Divide(904,654);
+}
+public static void main(String args[])
+{
+    Polymorphism test;
+    test=new Polymorphism();
+    test.Input();
+    test=new Multiply();
+    test.Input();
+    test=new Divide();
+    test.Input();
+}
+}
